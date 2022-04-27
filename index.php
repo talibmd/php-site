08a4132665bd1b8d49dfad7eef9f031ae37cb631
talib <talibjsss@gmail.com>
2022-04-27 16:35:35 +0530

    index page modified

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?
-echo "Helllo in the world of php";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width='', initial-scale=1.0">
+    <title>Php Primer - Index</title>
+</head>
+<body>
+    <h1>This is HTML <h1>H1</h1> tag</h1>
+    <?php
+        echo "This is a php script code. This line is printed using echo";
+    ?>
+</body>
+</html>
